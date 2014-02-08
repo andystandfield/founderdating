af485b9a2278cc852080ca71ec5f2de02beab99f
Andy Standfield <vertigo25@gmail.com>
2014-02-08 12:49:00 -0800

    Sortable & responsive
    Results now sort and grid is responsive.

--- test2/js/main.js
+++ test2/js/main.js
@@ -1,30 +1,68 @@
-$(document).ready(function(){
 
-	$.getJSON("/js/people.json", function(data) {
+function peopleResults(sort){
+	$.getJSON( "js/people.json", function( data ) {
+		var people = [];
+		$.each( data.people, function( i, val ) {
+			people.push( val );
+		});
 
-		$.each( data.people, function( i ) {
-			var pID = data.people[ i ].id;
-			var pName = data.people[ i ].name;
-			var pLocation = data.people[ i ].location;
-			var pPictureURL = data.people[ i ].picture;
-			var pInterests = data.people[ i ].interests;
+		people.sort(function(a, b){
+		    return b.id - a.id;
+		});
 
-			$("<div class='person_card' id='" + pID + "' data-ID='" + pID + "'></div>").appendTo(".results");
+		if (sort === "old") {
+			people.sort(function(a, b){
+			    return a.id - b.id;
+			});
+		}
+		
+		$("#search_results").html("");
+
+		$.each( people, function( i ) {
+			var pID = people[ i ].id;
+			var pName = people[ i ].name;
+			var pLocation = people[ i ].location;
+			var pPictureURL = people[ i ].picture;
+			var pInterests = people[ i ].interests;
+
+			JSON.stringify(pInterests);
+
+			var pInterests_list = pInterests.split(",");
+
+			console.log("list: " + pInterests_list.length + " pInterests: " + pInterests.length);
+
+			$("<div class='person_card' id='" + pID + "' data-ID='" + pID + "'></div>").appendTo("#search_results");
 
 			$("<div class='card_header'></div>").appendTo("#" + pID);
 
 			if (pPictureURL === "") {
 				$("<img class='photo' src='/img/nopic.png'>").appendTo("#" + pID + " div.card_header");
 			} else {
-				$("<img class='photo' src='" + pPictureURL + "'>").appendTo("#" + pID + " div.card_header");
+				$("<img class='photo' src='" + pPictureURL + "' onerror='this.src=\"/img/nopic.png\"'>").appendTo("#" + pID + " div.card_header");
 			}
 
 			$("<h2 class='name'>" + pName + "</h2>").appendTo("#" + pID + " .card_header");
 			$("<h3 class='location'>" + pLocation + "</h3>").appendTo("#" + pID + " .card_header");
 			$("<h3 class='interests_header'>Interests</h2>").appendTo("#" + pID);
-			$("<div class='interests_list'>" + pInterests + "</d>").appendTo("#" + pID);
+			$("<div class='interests_list'><ul></ul></div>").appendTo("#" + pID);
+			$.each( pInterests_list, function( interest ) {
+				$("<li>" + pInterests_list[interest] + "</li>").appendTo("#" + pID + " .interests_list ul");
+			});
 		});
+
+		console.log(people.length);
+	});
+
+}
+
+$(document).ready(function(){
+
+	peopleResults();
+
+	$( "#sort_order" ).change(function() {
+		peopleResults($( "#sort_order" ).val());
 	});
 
 });
 
+ 
